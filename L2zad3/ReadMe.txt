f91590349301741528da41ed68ad22a56c17d204
Celcik <lukasz.czerniszewski@gmail.com>
2021-04-23 16:01:40 +0200

    Finished

---
+++ L2zad3/ReadMe.txt
@@ -0,0 +1,6 @@
+w celu kompilacji 
+Musimy wykonać 1 plik make po 2 kompilacje programu proces3.c (gcc -o p3 proces3.c)
+Kolejnosc urochomiania: 
+1.Proces 3 (./p3)
+2.Server ./app_server
+3.Klinet z parametrem (localhost) ./app_client localhost
--- L2zad3/app_server.c
+++ L2zad3/app_server.c
@@ -6,6 +6,20 @@
 
 #include "app.h"
 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+  
+#define PORT     8080
+#define MAXLINE 1024
+
+
 wyjscie *
 obliczenia_1_svc(wejscie *argp, struct svc_req *rqstp)
 {
@@ -45,6 +59,47 @@ obliczenia_1_svc(wejscie *argp, struct svc_req *rqstp)
 	//result.suma    = argp->x1 + argp->x2;
 	//result.roznica = argp->x1 - argp->x2;
 	//result.iloczyn = argp->x1 * argp->x2;
+
+
+
+    int sockfd;
+    char buffer[MAXLINE];
+    char *hello = t;
+    struct sockaddr_in     servaddr;
+  
+    // Creating socket file descriptor
+    if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+        perror("socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+  
+    memset(&servaddr, 0, sizeof(servaddr));
+      
+    // Filling server information
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = INADDR_ANY;
+      
+    int n, len;
+      
+    sendto(sockfd, (const char *)hello, strlen(hello),
+        MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
+            sizeof(servaddr));
+    // printf("Hello message sent.\n");
+          
+    // n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
+    //             MSG_WAITALL, (struct sockaddr *) &servaddr,
+    //             &len);
+    // buffer[n] = '\0';
+    // printf("Server : %s\n", buffer);
+  
+    close(sockfd);
+
+
+
+
+
+
 
 	return &result;
 }
